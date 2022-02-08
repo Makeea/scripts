8693f3d6663bce971a8861bfb4c42cd7c2e4413b
Claire R <clairerosario1@gmail.com>
2022-02-08 09:21:30 +0000

    install nodejs

--- linux/nodejs.sh
+++ linux/nodejs.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 # install nvm, node, npm
- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash # Install nvm
- export NVM_DIR="$HOME/.nvm" # add NVM to the path
- [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
- [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # this loads bash completion for nvm
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash # Install nvm
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+source ~/.bashrc
  nvm install --lts # install latest node LTS module
+
+
+
+nvm install node 16
+nvm use node 16
+#Check Node Ver
+ nvm --version
+ npm -version
+ nvm current
