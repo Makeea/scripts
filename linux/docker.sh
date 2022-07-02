6c21fa31f8ccff920e3f2e339fe5e221c12268c4
Claire R <clairerosario1@gmail.com>
2022-07-02 08:58:48 +0000

    fix docker files

--- linux/docker.sh
+++ linux/docker.sh
@@ -20,14 +20,3 @@ sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
-
-# Now Install Portainer
-
-sudo docker run -d \
---name="portainer" \
---restart on-failure \
--p 9000:9000 \
--p 8000:8000 \
--v /var/run/docker.sock:/var/run/docker.sock \
--v portainer_data:/data \
-portainer/portainer-ce:latest
