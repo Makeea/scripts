19c8a4b9a9865ca46629e7b3df0286e3320037ec
Claire R <clairerosario1@gmail.com>
2022-07-02 01:32:11 -0400

    docker and portainer setup

---
+++ linux/docker_portainer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# update the system
+
+# update Ubuntu
+sudo apt update -y # update the package versions
+sudo apt upgrade -y # upgrade installed packages to new versions if was found
+apt list --upgradable
+sudo apt list --upgradable
+sudo apt autoremove -y
+
+# Update System
+
+sudo apt update
+sudo apt upgrade
+
+# Install Docker
+
+sudo apt install docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo systemctl status docker
+
+# Now Install Portainer
+
+sudo docker run -d \
+--name="portainer" \
+--restart on-failure \
+-p 9000:9000 \
+-p 8000:8000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data \
+portainer/portainer-ce:latest
