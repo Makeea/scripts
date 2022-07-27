4c48b74ac17104ceb9941d310706b815c347a221
Claire R <clairerosario1@gmail.com>
2022-07-27 06:07:26 +0000

    uninstall a Desktop Environment/GUI in Ubuntu Server

--- linux/ubuntu-desktop-GNOME-remove.sh
+++ linux/ubuntu-desktop-GNOME-remove.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# update the system
-
-# update Ubuntu
-sudo apt update -y # update the package versions
-sudo apt upgrade -y # upgrade installed packages to new versions if was found
-apt list --upgradable
-sudo apt list --upgradable
-sudo apt autoremove -y
-
-# Update System
-
-sudo apt update
-sudo apt upgrade
-
 # Uninstall GNOME
 sudo apt autoremove ubuntu-desktop
 
@@ -21,3 +7,5 @@ sudo apt autoremove ubuntu-desktop
 # Uninstall LightDM
 sudo systemctl stop lightdm.service
 sudo apt autoremove lightdm
+
+sudo apt autoremove -y
