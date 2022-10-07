2ec0005af77986edb2e15340228bc9ad9cdcd1dd
Claire R <clairerosario1@gmail.com>
2022-10-07 09:32:36 -0400

    fix for the untiy control center - aka - ubuntu desktop settings

---
+++ linux/unity-control-center-fix.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Uninstall and then reinstall System Settings (unity-control-center) from the Ubuntu Software Center application.
+
+# uninstall control center aka settings on ubuntu desktop
+sudo apt-get remove unity-control-center -y
+
+# update Ubuntu
+sudo apt update -y # update the package versions
+sudo apt upgrade -y # upgrade installed packages to new versions if was found
+apt list --upgradable
+sudo apt list --upgradable
+sudo apt autoremove -y
+
+# Update System
+
+sudo apt update
+sudo apt upgrade
+
+# reinstall unity-control-center
+
+sudo apt-get install unity-control-center -y
+
+sudo reboot
