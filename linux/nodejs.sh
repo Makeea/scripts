6dbe4d6dbe239e391358004b63d3b40e24a49976
Claire R <@gmail.com>
2023-12-01 03:18:55 -0500

    nodejs script update

--- linux/nodejs.sh
+++ linux/nodejs.sh
@@ -1,17 +1,41 @@
 #!/bin/sh
 
-# install nvm, node, npm
- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash # Install nvm
-  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# Script Description:
+# This script installs NVM (Node Version Manager) and then allows the user to choose 
+# a specific version of Node.js to install and use.
+
+# Install NVM (Node Version Manager)
+echo "Installing NVM (Node Version Manager)..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Set up NVM environment variables
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Reload .bashrc to apply NVM settings
 source ~/.bashrc
- nvm install --lts # install latest node LTS module
 
+# Install the latest Node LTS (Long Term Support) version
+echo "Installing the latest Node.js LTS version..."
+nvm install --lts
 
+# Prompt the user for a specific Node.js version to install
+echo "Enter the Node.js version you wish to install (e.g., '16', '14.17.0'): "
+read NODE_VERSION
 
-nvm install node 16
-nvm use node 16
-#Check Node Ver
- nvm --version
- npm -version
- nvm current
+# Install and use the user-specified version of Node.js
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+
+# Display the installed versions of NVM, Node.js, and NPM
+echo "NVM version:"
+nvm --version
+
+echo "Node.js version:"
+node --version
+
+echo "NPM version:"
+npm -version
+
+echo "Current Node.js version in use:"
+nvm current
